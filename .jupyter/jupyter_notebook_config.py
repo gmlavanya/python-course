11376234b89e5270cae16f0a1dee9f95a5cdbd0b
gmlavanya <lavanya.gmoorthy@gmail.com>
2025-01-23 23:13:58 +0100

    added hased value

--- .jupyter/jupyter_notebook_config.py
+++ .jupyter/jupyter_notebook_config.py
@@ -1,6 +1,6 @@
 from notebook.auth import passwd
 c = get_config()
-c.NotebookApp.password = passwd("@Welcome2024")
+c.NotebookApp.password = passwd("8$mFbOiDfQVbn3k2AxQIF2fQ$bZ0CkZZIBW4YK+TTehiUBv9qgQc6h7HFxceitKCQshM")
 c.NotebookApp.open_browser = False
 c.NotebookApp.ip = '0.0.0.0'
 c.NotebookApp.port = 8888
