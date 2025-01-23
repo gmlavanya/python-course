b5c922a0678d6fb1be5e747126eb2fd929bd0595
gmlavanya <163423145+gmlavanya@users.noreply.github.com>
2025-01-23 23:24:23 +0100

    Update jupyter_notebook_config.py

--- .jupyter/jupyter_notebook_config.py
+++ .jupyter/jupyter_notebook_config.py
@@ -1,6 +1,6 @@
 from notebook.auth import passwd
 c = get_config()
-c.NotebookApp.password = passwd("8$mFbOiDfQVbn3k2AxQIF2fQ$bZ0CkZZIBW4YK+TTehiUBv9qgQc6h7HFxceitKCQshM")
+c.NotebookApp.password = passwd("sha1:5a52a62c0dc47a322e7d8169c70d1c529c3e8932")
 c.NotebookApp.open_browser = False
 c.NotebookApp.ip = '0.0.0.0'
 c.NotebookApp.port = 8888
