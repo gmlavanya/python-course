b8587a5c80cd995436c1dee2d52081ac6238efbe
gmlavanya <163423145+gmlavanya@users.noreply.github.com>
2025-01-24 21:39:11 +0100

    Delete .jupyter directory

--- .jupyter/jupyter_notebook_config.py
+++
@@ -1,6 +0,0 @@
-from notebook.auth import passwd
-c = get_config()
-c.NotebookApp.password = passwd("sha1:5a52a62c0dc47a322e7d8169c70d1c529c3e8932")
-c.NotebookApp.open_browser = False
-c.NotebookApp.ip = '0.0.0.0'
-c.NotebookApp.port = 8888
